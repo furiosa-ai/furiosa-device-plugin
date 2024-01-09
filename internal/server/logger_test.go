3880c7473c38c581d47cb1ba4303f0e3ee9c1375
bg-furiosa <bg.chun@furiosa.ai>
2024-01-09 16:27:28 +0900

    add device-plugin grpc server
    Signed-off-by: bg-furiosa <bg.chun@furiosa.ai>

---
+++ internal/server/logger_test.go
@@ -0,0 +1,4 @@
+package server
+
+//TODO(@bg): write simple unittest for getNewErrorEventLogger and getNewDebugEventLogger
+// https://github.com/philip-bui/grpc-zerolog/blob/master/util_test.go
---
+++ internal/server/server_test.go
@@ -0,0 +1,10 @@
+package server
+
+/*
+Test grpc handlers
+GetDevicePluginOptions(context.Context, *Empty) (*DevicePluginOptions, error)
+ListAndWatch(*Empty, DevicePlugin_ListAndWatchServer) error
+GetPreferredAllocation(context.Context, *PreferredAllocationRequest) (*PreferredAllocationResponse, error)
+Allocate(context.Context, *AllocateRequest) (*AllocateResponse, error)
+PreStartContainer(context.Context, *PreStartContainerRequest) (*PreStartContainerResponse, error)
+*/
