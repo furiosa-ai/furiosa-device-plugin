129b5d26767c48b374eb2e05914a5ab8a2b875eb
Kihoon Han <kihoon.han@furiosa.ai>
2024-10-23 17:50:05 +0900

    Uncomment `TestID_RNGD_PartitionedDevice(...)` and `TestTopologyHintKey_RNGD_PartitionedDevice(...)`

--- internal/device_manager/partitioned_device_rngd_test.go
+++ internal/device_manager/partitioned_device_rngd_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/furiosa-ai/furiosa-device-plugin/internal/config"
 	"github.com/furiosa-ai/furiosa-smi-go/pkg/smi"
+	"github.com/furiosa-ai/libfuriosa-kubernetes/pkg/npu_allocator"
 	"github.com/stretchr/testify/assert"
 	devicePluginAPIv1Beta1 "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
 )
@@ -731,171 +732,171 @@ func TestMounts_RNGD_PartitionedDevice(t *testing.T) {
 	}
 }
 
-//func TestID_RNGD_PartitionedDevice(t *testing.T) {
-//	rngdMockDevice := smi.GetStaticMockDevices(smi.ArchRngd)[0]
-//	rngdMockDeviceUUID := "A76AAD68-6855-40B1-9E86-D080852D1C80"
-//
-//	tests := []struct {
-//		description     string
-//		mockDevice      smi.Device
-//		strategy        config.ResourceUnitStrategy
-//		expectedResults []string
-//	}{
-//		{
-//			description: "should return a list of RNGD Device ID for single core strategy",
-//			mockDevice:  rngdMockDevice,
-//			strategy:    config.SingleCoreStrategy,
-//			expectedResults: []string{
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "0"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "1"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "2"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "3"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "4"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "5"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "6"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "7"),
-//			},
-//		},
-//		{
-//			description: "should return a list of RNGD Device ID for dual core strategy",
-//			mockDevice:  rngdMockDevice,
-//			strategy:    config.DualCoreStrategy,
-//			expectedResults: []string{
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "0-1"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "2-3"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "4-5"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "6-7"),
-//			},
-//		},
-//		{
-//			description: "should return a list of RNGD Device ID for quad core strategy",
-//			mockDevice:  rngdMockDevice,
-//			strategy:    config.QuadCoreStrategy,
-//			expectedResults: []string{
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "0-3"),
-//				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "4-7"),
-//			},
-//		},
-//	}
-//
-//	for _, tc := range tests {
-//		partitionedDevices, err := NewPartitionedDevices(tc.mockDevice, tc.strategy, false)
-//		if err != nil {
-//			t.Errorf("%s: unexpected error: %v", tc.description, err)
-//			continue
-//		}
-//
-//		if len(partitionedDevices) != len(tc.expectedResults) {
-//			t.Errorf("length of expectedResults and partitioned devices are not equal for strategy %s: expected: %d, got: %d", tc.strategy, len(tc.expectedResults), len(partitionedDevices))
-//			continue
-//		}
-//
-//		for i, device := range partitionedDevices {
-//			expectedId := tc.expectedResults[i]
-//			actualId := device.ID()
-//			if expectedId != actualId {
-//				t.Errorf("expected ID %s, got %s", expectedId, actualId)
-//				continue
-//			}
-//		}
-//	}
-//}
+func TestID_RNGD_PartitionedDevice(t *testing.T) {
+	rngdMockDevice := smi.GetStaticMockDevices(smi.ArchRngd)[0]
+	rngdMockDeviceUUID := "A76AAD68-6855-40B1-9E86-D080852D1C80"
 
-//func TestTopologyHintKey_RNGD_PartitionedDevice(t *testing.T) {
-//	rngdMockDevice0 := smi.GetStaticMockDevices(smi.ArchRngd)[0]
-//	rngdMockDevice0PciBusId := "27"
-//
-//	rngdMockDevice1 := smi.GetStaticMockDevices(smi.ArchRngd)[1]
-//	rngdMockDevice1PciBusId := "2a"
-//
-//	tests := []struct {
-//		description    string
-//		mockDevice     smi.Device
-//		strategy       config.ResourceUnitStrategy
-//		expectedResult string
-//	}{
-//		{
-//			description:    "returned devices must have same TopologyHintKeys - RNGD 0",
-//			mockDevice:     rngdMockDevice0,
-//			strategy:       config.SingleCoreStrategy,
-//			expectedResult: rngdMockDevice0PciBusId,
-//		},
-//		{
-//			description:    "returned devices must have same TopologyHintKeys - RNGD 1",
-//			mockDevice:     rngdMockDevice1,
-//			strategy:       config.SingleCoreStrategy,
-//			expectedResult: rngdMockDevice1PciBusId,
-//		},
-//	}
-//
-//	for _, tc := range tests {
-//		partitionedDevices, err := NewPartitionedDevices(tc.mockDevice, tc.strategy, false)
-//		if err != nil {
-//			t.Errorf("%s: unexpected error: %v", tc.description, err)
-//			continue
-//		}
-//
-//		for _, device := range partitionedDevices {
-//			actualResult := device.TopologyHintKey()
-//			if actualResult != tc.expectedResult {
-//				t.Errorf("expectedResults %s, got %s", tc.expectedResult, actualResult)
-//				continue
-//			}
-//		}
-//	}
-//}
+	tests := []struct {
+		description     string
+		mockDevice      smi.Device
+		strategy        config.ResourceUnitStrategy
+		expectedResults []string
+	}{
+		{
+			description: "should return a list of RNGD Device ID for single core strategy",
+			mockDevice:  rngdMockDevice,
+			strategy:    config.SingleCoreStrategy,
+			expectedResults: []string{
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "0"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "1"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "2"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "3"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "4"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "5"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "6"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "7"),
+			},
+		},
+		{
+			description: "should return a list of RNGD Device ID for dual core strategy",
+			mockDevice:  rngdMockDevice,
+			strategy:    config.DualCoreStrategy,
+			expectedResults: []string{
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "0-1"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "2-3"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "4-5"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "6-7"),
+			},
+		},
+		{
+			description: "should return a list of RNGD Device ID for quad core strategy",
+			mockDevice:  rngdMockDevice,
+			strategy:    config.QuadCoreStrategy,
+			expectedResults: []string{
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "0-3"),
+				fmt.Sprintf("%s%s%s", rngdMockDeviceUUID, deviceIdDelimiter, "4-7"),
+			},
+		},
+	}
 
-//func TestEqual_RNGD_PartitionedDevice(t *testing.T) {
-//	tests := []struct {
-//		description      string
-//		mockSourceDevice smi.Device
-//		mockTargetDevice smi.Device
-//		strategy         config.ResourceUnitStrategy
-//		expected         bool
-//	}{
-//		{
-//			description:      "expect source and target are identical",
-//			mockSourceDevice: smi.GetStaticMockDevices(smi.ArchRngd)[0],
-//			mockTargetDevice: smi.GetStaticMockDevices(smi.ArchRngd)[0],
-//			strategy:         config.SingleCoreStrategy,
-//			expected:         true,
-//		},
-//		{
-//			description:      "expect source and target are not identical",
-//			mockSourceDevice: smi.GetStaticMockDevices(smi.ArchRngd)[0],
-//			mockTargetDevice: smi.GetStaticMockDevices(smi.ArchRngd)[1],
-//			strategy:         config.SingleCoreStrategy,
-//			expected:         false,
-//		},
-//	}
-//
-//	for _, tc := range tests {
-//		sourcePartitionedDevices, err := NewPartitionedDevices(tc.mockSourceDevice, tc.strategy, false)
-//		if err != nil {
-//			t.Errorf("%s: unexpected error: %v", tc.description, err)
-//			continue
-//		}
-//
-//		targetPartitionedDevices, err := NewPartitionedDevices(tc.mockTargetDevice, tc.strategy, false)
-//		if err != nil {
-//			t.Errorf("%s: unexpected error: %v", tc.description, err)
-//			continue
-//		}
-//
-//		if len(sourcePartitionedDevices) != len(targetPartitionedDevices) {
-//			t.Errorf("length of sourcePartitionedDevices and targetPartitionedDevices is different!")
-//			continue
-//		}
-//
-//		for i := range sourcePartitionedDevices {
-//			sourceDevice := sourcePartitionedDevices[i]
-//			targetDevice := targetPartitionedDevices[i]
-//
-//			actualResult := sourceDevice.Equal(targetDevice)
-//			if actualResult != tc.expected {
-//				t.Errorf("expected: %v, got: %v", tc.expected, actualResult)
-//				continue
-//			}
-//		}
-//	}
-//}
+	for _, tc := range tests {
+		partitionedDevices, err := NewPartitionedDevices(0, tc.mockDevice, tc.strategy, false)
+		if err != nil {
+			t.Errorf("%s: unexpected error: %v", tc.description, err)
+			continue
+		}
+
+		if len(partitionedDevices) != len(tc.expectedResults) {
+			t.Errorf("length of expectedResults and partitioned devices are not equal for strategy %s: expected: %d, got: %d", tc.strategy, len(tc.expectedResults), len(partitionedDevices))
+			continue
+		}
+
+		for i, device := range partitionedDevices {
+			expectedId := tc.expectedResults[i]
+			actualId := device.ID()
+			if expectedId != actualId {
+				t.Errorf("expected ID %s, got %s", expectedId, actualId)
+				continue
+			}
+		}
+	}
+}
+
+func TestTopologyHintKey_RNGD_PartitionedDevice(t *testing.T) {
+	rngdMockDevice0 := smi.GetStaticMockDevices(smi.ArchRngd)[0]
+	rngdMockDevice0PciBusId := "27"
+
+	rngdMockDevice1 := smi.GetStaticMockDevices(smi.ArchRngd)[1]
+	rngdMockDevice1PciBusId := "2a"
+
+	tests := []struct {
+		description    string
+		mockDevice     smi.Device
+		strategy       config.ResourceUnitStrategy
+		expectedResult npu_allocator.TopologyHintKey
+	}{
+		{
+			description:    "returned devices must have same TopologyHintKeys - RNGD 0",
+			mockDevice:     rngdMockDevice0,
+			strategy:       config.SingleCoreStrategy,
+			expectedResult: npu_allocator.TopologyHintKey(rngdMockDevice0PciBusId),
+		},
+		{
+			description:    "returned devices must have same TopologyHintKeys - RNGD 1",
+			mockDevice:     rngdMockDevice1,
+			strategy:       config.SingleCoreStrategy,
+			expectedResult: npu_allocator.TopologyHintKey(rngdMockDevice1PciBusId),
+		},
+	}
+
+	for _, tc := range tests {
+		partitionedDevices, err := NewPartitionedDevices(0, tc.mockDevice, tc.strategy, false)
+		if err != nil {
+			t.Errorf("%s: unexpected error: %v", tc.description, err)
+			continue
+		}
+
+		for _, device := range partitionedDevices {
+			actualResult := device.TopologyHintKey()
+			if actualResult != tc.expectedResult {
+				t.Errorf("expectedResults %s, got %s", tc.expectedResult, actualResult)
+				continue
+			}
+		}
+	}
+}
+
+func TestEqual_RNGD_PartitionedDevice(t *testing.T) {
+	tests := []struct {
+		description      string
+		mockSourceDevice smi.Device
+		mockTargetDevice smi.Device
+		strategy         config.ResourceUnitStrategy
+		expected         bool
+	}{
+		{
+			description:      "expect source and target are identical",
+			mockSourceDevice: smi.GetStaticMockDevices(smi.ArchRngd)[0],
+			mockTargetDevice: smi.GetStaticMockDevices(smi.ArchRngd)[0],
+			strategy:         config.SingleCoreStrategy,
+			expected:         true,
+		},
+		{
+			description:      "expect source and target are not identical",
+			mockSourceDevice: smi.GetStaticMockDevices(smi.ArchRngd)[0],
+			mockTargetDevice: smi.GetStaticMockDevices(smi.ArchRngd)[1],
+			strategy:         config.SingleCoreStrategy,
+			expected:         false,
+		},
+	}
+
+	for _, tc := range tests {
+		sourcePartitionedDevices, err := NewPartitionedDevices(0, tc.mockSourceDevice, tc.strategy, false)
+		if err != nil {
+			t.Errorf("%s: unexpected error: %v", tc.description, err)
+			continue
+		}
+
+		targetPartitionedDevices, err := NewPartitionedDevices(0, tc.mockTargetDevice, tc.strategy, false)
+		if err != nil {
+			t.Errorf("%s: unexpected error: %v", tc.description, err)
+			continue
+		}
+
+		if len(sourcePartitionedDevices) != len(targetPartitionedDevices) {
+			t.Errorf("length of sourcePartitionedDevices and targetPartitionedDevices is different!")
+			continue
+		}
+
+		for i := range sourcePartitionedDevices {
+			sourceDevice := sourcePartitionedDevices[i]
+			targetDevice := targetPartitionedDevices[i]
+
+			actualResult := sourceDevice.Equal(targetDevice)
+			if actualResult != tc.expected {
+				t.Errorf("expected: %v, got: %v", tc.expected, actualResult)
+				continue
+			}
+		}
+	}
+}
